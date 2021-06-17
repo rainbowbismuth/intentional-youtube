533e481681627393fab1c889d0f486489919e2c2
Emily Bellows <emily.a.bellows@hey.com>
2021-06-16 23:18:27 -0400

    ah, the initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# intentional-youtube
+
+[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
+
+This is a web extension addon that removes the more distractible parts of [YouTube](https://www.youtube.com). This includes the homepage video suggestions, related videos, and all comments. But you can still go to a channel page and find videos that way, or explicitly search.
+
+It is intentionally minimal so you can read it and install it yourself.
+
+## Why not use `$other_thing`?
+
+I don't know. Is it 25 lines of commented code? Maybe you should.
+
+## Contributing
+
+Pull requests welcome if it doesn't make the code any harder to review in 5 seconds.
---
+++ intentional-youtube.js
@@ -0,0 +1,25 @@
+(function(){
+    const deleteStuffOnPage = function() {
+        const thingsToGetOuttaHere = [
+            "secondary", // Video sidebar, 'prolly other things too.
+            "related",   // Related videos.
+            "chips",     // The recommended video topics on homepage & elsewhere.
+            "comments"   // Seemingly broken by the above, but this removes your face. Nice.
+        ];
+        const onHomepage = document.URL.endsWith("youtube.com/");
+        if (onHomepage) {
+            thingsToGetOuttaHere.push("primary"); // The big video suggestion grid.
+        }
+        for (const id of thingsToGetOuttaHere) {
+            const elem = document.getElementById(id);
+            if (elem) {
+                elem.replaceWith(""); // Get outta here.
+            }
+        }
+    };
+    const observer = new MutationObserver(function(_mutationsList, _observer) {
+        deleteStuffOnPage();
+    });
+    // Tell me -EVERYTHING-.
+    observer.observe(document.body, { attributes: false, childList: true, subtree: true });
+})();
